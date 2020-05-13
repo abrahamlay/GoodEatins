edeaff7134d0681b6753665c1993f5b6da6b7173
abrahamlay <abraham_lay@yahoo.co.id>
2020-05-13 20:39:03 +0700

    Initial Commit

---
+++ GoodEatins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodEatins
+//
+//  Created by Abraham Lay (ID) on 13/05/20.
+//  Copyright © 2020 Abraham Lay (ID). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
